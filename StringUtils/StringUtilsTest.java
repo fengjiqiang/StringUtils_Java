e7344aebcd326fe4c8128f68792e70e0d0da79d2
Jiqiang Feng <feng1187932281@163.com>
2018-08-29 16:52:43 +0800

    Create StringUtilsTest.java

---
+++ StringUtils/StringUtilsTest.java
@@ -0,0 +1,9 @@
+package d20180829.ch07;
+
+public class StringUtilsTest {
+
+    public static void main(String[] args){
+        System.out.println(StringUtils.isPalindrome("abcba"));
+        System.out.println(StringUtils.isPalindrome("abcbaa"));
+    }
+}
