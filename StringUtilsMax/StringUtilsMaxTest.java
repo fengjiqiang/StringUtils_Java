f2f6018db6e3ce202d37436a11d351c3323ad278
Jiqiang Feng <feng1187932281@163.com>
2018-08-29 16:53:36 +0800

    Create StringUtilsMaxTest.java

---
+++ StringUtilsMax/StringUtilsMaxTest.java
@@ -0,0 +1,8 @@
+package d20180829.ch07;
+
+public class StringUtilsMaxTest {
+
+    public static void main(String[] args){
+        System.out.println(StringUtilsMax.longestPalindrome("abcbafedgttgdefaso"));
+    }
+}
